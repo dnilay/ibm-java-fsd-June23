7a22f8bce22cf3d094f88847c938eddc7db2865e
Nilay <54245048+dnilay@users.noreply.github.com>
2023-07-10 09:52:28 +0530

    Add files via upload

--- mapping-demo/src/main/java/org/example/App.java
+++ mapping-demo/src/main/java/org/example/App.java
@@ -23,20 +23,23 @@ public class App
         	SessionFactory sessionFactory=new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Employee.class).
         			addAnnotatedClass(Company.class).buildSessionFactory();
         	Session session=sessionFactory.openSession();
-        	Employee employee1=new Employee(new Random().nextInt(10000), "John", "Doe", "john@email.com");
-        	Employee employee2=new Employee(new Random().nextInt(10000), "Marry", "Public", "marry@email.com");
-        	Employee employee3=new Employee(new Random().nextInt(10000), "Rahul", "Dravid", "rahul@email.com");
-			Company ibm=new Company();
-			ArrayList<Employee> list=new ArrayList<Employee>();
-			list.add(employee1);
-			list.add(employee2);
-			list.add(employee3);
-			ibm.setCompanyId(UUID.randomUUID().toString());
-			ibm.setCompanyName("IBM India");
-			ibm.setCompanyAddress(new Address("Banglore", "Karnataka", "banglore", "54321"));
-			ibm.setEmployees(list);
+			/*
+			 * Employee employee1=new Employee(new Random().nextInt(10000), "John", "Doe",
+			 * "john@email.com"); Employee employee2=new Employee(new
+			 * Random().nextInt(10000), "Marry", "Public", "marry@email.com"); Employee
+			 * employee3=new Employee(new Random().nextInt(10000), "Rahul", "Dravid",
+			 * "rahul@email.com"); Company ibm=new Company(); ArrayList<Employee> list=new
+			 * ArrayList<Employee>(); list.add(employee1); list.add(employee2);
+			 * list.add(employee3); ibm.setCompanyId(UUID.randomUUID().toString());
+			 * ibm.setCompanyName("IBM India"); ibm.setCompanyAddress(new
+			 * Address("Banglore", "Karnataka", "banglore", "54321"));
+			 * ibm.setEmployees(list);
+			 */
 			session.getTransaction().begin();
-			session.save(ibm);
+			//session.save(ibm);
+			Company c=session.get(Company.class, "43908bf6-6a82-4c96-8e1e-a74920c89d87");
+			System.out.println(c);
+			session.remove(c);
 			session.getTransaction().commit();
 			System.out.println("company created...!");
 		} catch (Exception e) {
