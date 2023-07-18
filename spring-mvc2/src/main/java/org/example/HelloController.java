ce5572d82d60bdb17c74bc31a019757af26e8523
Nilay <54245048+dnilay@users.noreply.github.com>
2023-07-18 14:41:52 +0530

    Add files via upload

---
+++ spring-mvc2/src/main/java/org/example/HelloController.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping
+	public String goHome() {
+		return "home";
+	}
+}
