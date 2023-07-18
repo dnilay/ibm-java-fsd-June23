add67d094647e4a4e4243e3865f658ea50412011
Nilay <54245048+dnilay@users.noreply.github.com>
2023-07-18 15:48:49 +0530

    Add files via upload

--- spring-mvc2/src/main/java/org/example/HelloController.java
+++ spring-mvc2/src/main/java/org/example/HelloController.java
@@ -1,18 +1,32 @@
 package org.example;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.example.model.Employee;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HelloController {
 
-	@RequestMapping
+	@GetMapping
 	public String goHome() {
 		return "home";
 	}
 
-	@RequestMapping("/showForm")
+	@GetMapping("/showForm")
 	public String showForm() {
 		return "showForm";
+	}
+
+	@PostMapping("/addEmployee")
+	public String addEmployee(HttpServletRequest request, Model theModel) {
+		String name = request.getParameter("t1");
+		double age = Double.parseDouble(request.getParameter("t2"));
+		theModel.addAttribute("emp", new Employee(name, age));
+
+		return "sucess";
 	}
 }
--- spring-mvc2/src/main/webapp/WEB-INF/view/home.jsp
+++ spring-mvc2/src/main/webapp/WEB-INF/view/home.jsp
@@ -1,12 +1,28 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html>
 <html>
+
 <head>
-<meta charset="ISO-8859-1">
-<title>Insert title here</title>
+	<title>Employee: Home</title>
 </head>
-<body>
-welcome to spring mvc
+
+<body bgcolor="white">
+
+	<!-- Page Heading -->
+	<table border=’1’ cellpadding=’5’ cellspacing=’0’ width=’400’>
+		<tr bgcolor='red' align=’center’ valign=’center’ height=’20’>
+			<td>
+				<h3>Employee: Home</h3>
+			</td>
+		</tr>
+	</table>
+	<p>This is the Home page for Employee Management System .</p>
+
+	<ul>
+		<li><a href='#'>List all Employees</a></li>
+	
+		<li><a href="showForm">Add a new Employee</a></li>
+	</ul>
+
 </body>
+
 </html>
