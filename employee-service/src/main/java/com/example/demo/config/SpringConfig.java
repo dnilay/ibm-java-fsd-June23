df2077a62fc42da3aea4433a2267c93fd94031d4
Nilay <54245048+dnilay@users.noreply.github.com>
2023-07-31 14:19:48 +0530

    Add files via upload

--- employee-service/src/main/java/com/example/demo/config/SpringConfig.java
+++ employee-service/src/main/java/com/example/demo/config/SpringConfig.java
@@ -1,14 +1,11 @@
 package com.example.demo.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class SpringConfig {
 
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
+	/*
+	 * @Bean public RestTemplate restTemplate() { return new RestTemplate(); }
+	 */
 }
--- employee-service/src/main/java/com/example/demo/controller/EmployeeController.java
+++ employee-service/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -3,14 +3,16 @@ package com.example.demo.controller;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
+import com.example.demo.client.DepartmentClient;
+import com.example.demo.exception.DepartmentNoFountException;
 import com.example.demo.model.EmployeeEntity;
 import com.example.demo.service.EmployeeService;
 import com.example.demo.ui.DepartmentDto;
@@ -24,9 +26,21 @@ public class EmployeeController {
 
 	private final EmployeeService employeeService;
 	private final Environment environment;
+	private final DepartmentClient departmentClient;
+	// private final RestTemplate restTemplate;
 
-	private final RestTemplate restTemplate;
-
+	@ExceptionHandler
+	public ResponseEntity<?> handleException(DepartmentNoFountException e)
+	{
+		e.setErrorReportringTime(System.currentTimeMillis());
+		e.setStatusCode(HttpStatus.NOT_FOUND.value());
+		
+		StringBuffer sb=new StringBuffer();
+		sb.append(e.getMessaage());
+		sb.append(e.getErrorReportringTime());
+		sb.append(e.getStatusCode());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(sb);
+	}
 	@GetMapping("/status")
 	public ResponseEntity<?> status() {
 		return ResponseEntity
@@ -34,12 +48,14 @@ public class EmployeeController {
 	}
 
 	@PostMapping("/{departmentName}")
-	public ResponseEntity<?> createEmployee(@RequestBody EmployeeEntity employee,@PathVariable("departmentName") String departmentName) {
+	public ResponseEntity<?> createEmployee(@RequestBody EmployeeEntity employee,
+			@PathVariable("departmentName") String departmentName) {
 
-		ResponseEntity<DepartmentDto> o = restTemplate.getForEntity("http://localhost:9999/department-service/departments/" + departmentName,
-				DepartmentDto.class);
-
-		DepartmentDto dto = o.getBody();
+		DepartmentDto dto = departmentClient.getDepartmentByName(departmentName);
+		if(dto==null)
+		{
+			throw new DepartmentNoFountException("department with "+departmentName+" not found");
+		}
 		employee.setDepartmentId(dto.getDepartmentId());
 		employee.setDepartmentName(dto.getDepartmentName());
 		return ResponseEntity.status(HttpStatus.CREATED).body(employeeService.createEmployee(employee));
