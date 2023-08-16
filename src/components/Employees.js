a5a757566b6243d789944593f0d97fc8978bb87d
Nilay <54245048+dnilay@users.noreply.github.com>
2023-08-16 14:06:09 +0530

    Add files via upload

---
+++ src/components/Employees.js
@@ -0,0 +1,12 @@
+import './Employees.css'
+function Employees()
+{
+    return (
+        <div>
+          <h2>Employees Component</h2>
+          
+        </div>
+      );
+     
+}
+export default Employees;
